237f1bd6269d75d6261be29b7fb71d46dcc57dc0
duydang2311 <34796192+duydang2311@users.noreply.github.com>
2025-10-10 10:48:17 +0700

    refactor: use C collation for status rank

---
+++ api/WebApp.Api/Migrations/20251010034753_020_ChangeStatusRankCollation.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WebApp.Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class _020_ChangeStatusRankCollation : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "rank",
+                table: "statuses",
+                type: "text",
+                nullable: false,
+                collation: "C",
+                oldClrType: typeof(string),
+                oldType: "text"
+            );
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "rank",
+                table: "statuses",
+                type: "text",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "text",
+                oldCollation: "C"
+            );
+        }
+    }
+}
--- api/WebApp.Infrastructure/Data/Configs/StatusConfiguration.cs
+++ api/WebApp.Infrastructure/Data/Configs/StatusConfiguration.cs
@@ -14,6 +14,7 @@ public sealed class StatusConfiguration : IEntityTypeConfiguration<Status>
         builder.Property(a => a.Category);
         builder.Property(a => a.Color);
         builder.Property(a => a.Description);
+        builder.Property(a => a.Rank).UseCollation("C");
 
         builder.HasOne(a => a.Project).WithMany(a => a.Statuses).HasForeignKey(a => a.ProjectId);
         builder.HasKey(a => a.Id);
