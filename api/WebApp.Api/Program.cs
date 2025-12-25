e60264416155cd53c64d19b62155bf828877cafa
duydang2311 <34796192+duydang2311@users.noreply.github.com>
2025-12-25 21:47:02 +0700

    feat: introduce cli args for some utilities

--- api/WebApp.Api/Program.cs
+++ api/WebApp.Api/Program.cs
@@ -1,11 +1,103 @@
+using System.Security.Cryptography;
 using FastEndpoints;
 using FluentValidation;
+using Microsoft.Extensions.Options;
 using Slugify;
 using WebApp.Api;
 using WebApp.Api.Middlewares;
 using WebApp.Api.Security;
 using WebApp.Api.Serialization;
 using WebApp.Infrastructure.Data;
+using WebApp.Infrastructure.Integrations;
+using WebApp.Infrastructure.Jwts;
+
+if (args.Contains("--gen-keypair"))
+{
+    var rsa = RSA.Create(2048);
+    Console.WriteLine(
+        "PRIVATE KEY: " + Environment.NewLine + rsa.ExportPkcs8PrivateKeyPem().Replace("\n", "\\n")
+    );
+    Console.WriteLine(
+        "PUBLIC KEY: "
+            + Environment.NewLine
+            + rsa.ExportSubjectPublicKeyInfoPem().Replace("\n", "\\n")
+    );
+    return 0;
+}
+
+if (args.Contains("--gen-alphabet"))
+{
+    Span<char> alphabet =
+    [
+        'a',
+        'b',
+        'c',
+        'd',
+        'e',
+        'f',
+        'g',
+        'h',
+        'i',
+        'j',
+        'k',
+        'l',
+        'm',
+        'n',
+        'o',
+        'p',
+        'q',
+        'r',
+        's',
+        't',
+        'u',
+        'v',
+        'w',
+        'x',
+        'y',
+        'z',
+        'A',
+        'B',
+        'C',
+        'D',
+        'E',
+        'F',
+        'G',
+        'H',
+        'I',
+        'J',
+        'K',
+        'L',
+        'M',
+        'N',
+        'O',
+        'P',
+        'Q',
+        'R',
+        'S',
+        'T',
+        'U',
+        'V',
+        'W',
+        'X',
+        'Y',
+        'Z',
+        '0',
+        '1',
+        '2',
+        '3',
+        '4',
+        '5',
+        '6',
+        '7',
+        '8',
+        '9',
+        '-',
+        '_',
+    ];
+    RandomNumberGenerator.Shuffle(alphabet);
+    Console.WriteLine(alphabet.ToString());
+    return 0;
+}
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -43,6 +135,23 @@ builder.Services.AddFastEndpoints(FastEndpointsConfiguration.ConfigureDiscovery)
 
 var app = builder.Build();
 
+if (args.Contains("--workers-assets-jwt"))
+{
+    Console.WriteLine(
+        JwtHelper.CreateToken(
+            app.Services.GetRequiredService<
+                IOptions<IntegrationKeysOptions>
+            >().Value.AssetWorkers.PrivateKeyPem,
+            iss: "api",
+            aud: "workers-assets",
+            iat: DateTime.UtcNow,
+            nbf: DateTime.UtcNow,
+            exp: DateTime.UtcNow.AddYears(1)
+        )
+    );
+    return 0;
+}
+
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
@@ -64,24 +173,6 @@ app.UseFastEndpoints(options =>
 
 ValidatorOptions.Global.LanguageManager.Enabled = false;
 
-// var rsa = RSA.Create(2048);
-// Console.WriteLine(rsa.ExportPkcs8PrivateKeyPem());
-// Console.WriteLine(rsa.ExportSubjectPublicKeyInfoPem());
-// return 0;
-// Console.WriteLine(
-//     JwtHelper.CreateToken(
-//         app.Services.GetRequiredService<
-//             IOptions<IntegrationKeysOptions>
-//         >().Value.AssetWorkers.PrivateKeyPem,
-//         iss: "api",
-//         aud: "workers-assets",
-//         iat: DateTime.UtcNow,
-//         nbf: DateTime.UtcNow,
-//         exp: DateTime.UtcNow.AddYears(1)
-//     )
-// );
-// return 0;
-
 var scopeFactory = app.Services.GetRequiredService<IServiceScopeFactory>();
 await using (var scope = scopeFactory.CreateAsyncScope())
 {
@@ -90,3 +181,5 @@ await using (var scope = scopeFactory.CreateAsyncScope())
 }
 
 await app.RunAsync().ConfigureAwait(false);
+
+return 0;
