810b600316ae26e1c6b324ecda770577727f6c5c
duydang2311 <34796192+duydang2311@users.noreply.github.com>
2026-04-14 06:28:22 +0700

    perf: improve vite-guard plugin

--- www/src/lib/plugins/vite-guard.ts
+++ www/src/lib/plugins/vite-guard.ts
@@ -10,26 +10,41 @@ export function guardPlugin() {
             prod = config.mode === 'production';
         },
         async transform(code: string, id: string) {
-            if (!id.endsWith('.ts') && !id.endsWith('.js') && !id.endsWith('.svelte')) return;
+            if (
+                (!id.endsWith('.js') && !id.endsWith('.ts') && !id.endsWith('.svelte')) ||
+                !code.includes('guard')
+            ) {
+                return null;
+            }
+
+            let s: MagicString | null = null;
             const result = await parse(id, code);
-            const s = new MagicString(code);
             const visitor = new Visitor({
                 CallExpression(decl) {
-                    if (decl.callee.type === 'Identifier' && decl.callee.name === 'guardNull') {
-                        if (prod) {
+                    if (decl.callee.type === 'Identifier') {
+                        if (decl.callee.name === 'guardNull') {
+                            s ??= new MagicString(code);
+                            if (prod) {
+                                s.remove(decl.start, decl.end);
+                                return;
+                            }
+                            const arg = decl.arguments[0];
+                            s.appendLeft(decl.end - 1, `, "${code.slice(arg.start, arg.end)}"`);
+                        } else if (decl.callee.name === 'guard' && prod) {
+                            s ??= new MagicString(code);
                             s.remove(decl.start, decl.end);
                             return;
                         }
-                        const arg = decl.arguments[0];
-                        s.appendLeft(decl.end - 1, `, "${code.slice(arg.start, arg.end)}"`);
                     }
                 },
             });
             visitor.visit(result.program);
-            return {
-                code: s.toString(),
-                map: s.generateMap({ hires: true }),
-            };
+            return s
+                ? {
+                      code: (s as MagicString).toString(),
+                      map: (s as MagicString).generateMap(),
+                  }
+                : null;
         },
     } satisfies Plugin;
 }
