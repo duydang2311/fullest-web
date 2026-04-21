8c4f8fc6d70da41f2c49f7697865fbe2a640d61b
duydang2311 <34796192+duydang2311@users.noreply.github.com>
2026-04-21 21:58:26 +0700

    refactor(www): add markdown, table extensions

--- www/src/lib/components/editor.svelte.ts
+++ www/src/lib/components/editor.svelte.ts
@@ -1,6 +1,16 @@
-import { Editor, type EditorOptions, type Extensions, type JSONContent } from '@tiptap/core';
+import {
+    Editor,
+    Extension,
+    type EditorOptions,
+    type Extensions,
+    type JSONContent,
+} from '@tiptap/core';
 import { ListKit } from '@tiptap/extension-list';
+import { TableKit } from '@tiptap/extension-table';
 import { Placeholder } from '@tiptap/extensions';
+import { Markdown } from '@tiptap/markdown';
+import { Slice } from '@tiptap/pm/model';
+import { Plugin, PluginKey } from '@tiptap/pm/state';
 import StarterKit from '@tiptap/starter-kit';
 import { renderToHTMLString as __renderToHTMLString } from '@tiptap/static-renderer';
 import { untrack } from 'svelte';
@@ -25,6 +35,9 @@ export function createExtensions(): Extensions {
                 nested: true,
             },
         }),
+        TableKit,
+        Markdown,
+        MarkdownClipboard,
     ];
 }
 
@@ -89,3 +102,42 @@ export function createEditor(options?: Omit<Partial<TextEditorOptions>, 'element
 export function renderToHTMLString(jsonContent: JSONContent) {
     return __renderToHTMLString({ content: jsonContent, extensions: createExtensions() });
 }
+
+export const MarkdownClipboard = Extension.create({
+    name: 'markdownClipboard',
+    addOptions: () => ({
+        transformPastedText: true,
+        transformCopiedText: true,
+    }),
+    addProseMirrorPlugins() {
+        return [
+            new Plugin({
+                key: new PluginKey('markdownClipboard'),
+                props: {
+                    clipboardTextParser: (text, _context, plainText) => {
+                        if (
+                            plainText ||
+                            !this.options.transformPastedText ||
+                            !this.editor.markdown
+                        ) {
+                            return null as any;
+                        }
+                        const parsedJson = this.editor.markdown.parse(text);
+                        const doc = this.editor.schema.nodeFromJSON(parsedJson);
+                        return new Slice(doc.content, 1, 1);
+                    },
+
+                    clipboardTextSerializer: (slice) => {
+                        if (!this.options.transformCopiedText || !this.editor.markdown) {
+                            return null as any;
+                        }
+                        const sliceJson = slice.toJSON();
+                        sliceJson.type = 'doc';
+                        const md = this.editor.markdown.serialize(sliceJson);
+                        return md;
+                    },
+                },
+            }),
+        ];
+    },
+});
