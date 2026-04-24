6ea6340da9a5b45f397be459d7903e1d043f9833
duydang2311 <34796192+duydang2311@users.noreply.github.com>
2026-04-24 18:30:34 +0700

    chore: clean up

--- www/src/routes/(auth-optional)/+page.server.ts
+++ www/src/routes/(auth-optional)/+page.server.ts
@@ -1,12 +1,9 @@
-import { error } from '@sveltejs/kit';
 import { reverseKeysetList } from '~/lib/models/paginated';
-import { Err } from '~/lib/utils/errors';
 import { observableServerLoad } from '~/lib/utils/observability';
 import type { PageServerLoad } from './$types';
 import { getProjectList } from './_page/utils.svelte';
 
 export const load = observableServerLoad<PageServerLoad>(async (e) => {
-    error(500, Err('ERR_MISSING_OAUTH_STATE'));
     const session = e.locals.session;
     if (session) {
         const lastId = e.untrack(() => e.url.searchParams.get('p'));
