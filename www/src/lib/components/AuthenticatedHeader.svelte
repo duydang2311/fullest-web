1016b5f31aff4cee70574fbe895374cb87dcd0fc
duydang2311 <34796192+duydang2311@users.noreply.github.com>
2026-05-01 18:35:33 +0700

    refactor(www): remove header create actions

--- www/src/lib/components/AuthenticatedHeader.svelte
+++ www/src/lib/components/AuthenticatedHeader.svelte
@@ -1,7 +1,6 @@
 <script lang="ts">
     import type { UserPreset } from '../models/user';
     import { C } from '../utils/styles';
-    import CreateActions from './AuthenticatedHeaderCreateActions.svelte';
     import AuthenticatedHeaderPfp from './AuthenticatedHeaderPfp.svelte';
     import LogoType from './LogoType.svelte';
 
@@ -16,10 +15,9 @@
     <nav class="flex items-center justify-between gap-8">
         <div class="flex items-center gap-4">
             <LogoType />
-            <a href="/" class="{C.button({ variant: 'base', ghost: true, size: 'sm' })}">Home</a>
+            <a href="/" class={C.button({ variant: 'base', ghost: true, size: 'sm' })}>Home</a>
         </div>
         <div class="flex items-center gap-2">
-            <CreateActions />
             <AuthenticatedHeaderPfp {user} />
         </div>
     </nav>
