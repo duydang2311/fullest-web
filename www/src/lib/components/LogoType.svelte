b79a53a9f8b67b119fcfd22cf47b1caca4f9852f
duydang2311 <34796192+duydang2311@users.noreply.github.com>
2026-05-06 02:13:23 +0700

    style(www): shorten font family variable

--- www/src/lib/components/LogoType.svelte
+++ www/src/lib/components/LogoType.svelte
@@ -1,11 +1,11 @@
 <script lang="ts">
-	import type { HTMLAttributes } from 'svelte/elements';
-	import { Logo } from '~/lib/components/icons';
+    import type { HTMLAttributes } from 'svelte/elements';
+    import { Logo } from '~/lib/components/icons';
 
-	const { class: cls, ...props }: HTMLAttributes<HTMLDivElement> = $props();
+    const { class: cls, ...props }: HTMLAttributes<HTMLDivElement> = $props();
 </script>
 
 <div {...props} class="flex items-center gap-1 {cls}">
-	<Logo />
-	<p class="text-fg-emph font-black tracking-tight">FULLEST</p>
+    <Logo />
+    <p class="text-fg-emph font-black tracking-tight font-ui">FULLEST</p>
 </div>
