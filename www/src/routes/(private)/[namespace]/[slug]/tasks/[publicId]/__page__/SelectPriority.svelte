4c9cc26d97588caa0bb8088269986a3743b61674
duydang2311 <34796192+duydang2311@users.noreply.github.com>
2026-05-23 05:55:13 +0700

    refactor(www): no longer need to fetch only if null due to cache

--- www/src/routes/(private)/[namespace]/[slug]/tasks/[publicId]/__page__/SelectPriority.svelte
+++ www/src/routes/(private)/[namespace]/[slug]/tasks/[publicId]/__page__/SelectPriority.svelte
@@ -22,7 +22,7 @@
         defaultHighlightedValue: untrack(() => task.priority?.id),
         async onOpenChange(details) {
             if (details.open) {
-                priorityList ??= await usePriorityList();
+                priorityList = await usePriorityList();
                 menu.api.setHighlightedValue(task.priority?.id);
             }
         },
